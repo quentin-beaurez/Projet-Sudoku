8c906dbdbcc42b32c6db93a6b4b8ea321b38e6d0
Mohammed-Emir <bettahar.mohammedemir@gmail.com>
2026-01-20 10:34:03 +0100

    sunit definition

--- sudoku.hpp
+++ sudoku.hpp
@@ -9,12 +9,12 @@ using namespace std;
 // classe Grille ============================================
 //===========================================================
 
+typedef unsigned int suint;
 class Grille : vector<vector<int>>
 {public :
     int n = 0; // ordre de la grille
     mutable vector<pair<suint,suint>> casesVides; // vecteur des cases vides
     mutable vector<suint> vs,vl,vc,vb; // vecteurs de travail
 }
-
 
 #endif 
