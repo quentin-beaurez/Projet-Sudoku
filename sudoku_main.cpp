25638ded7025e29f4b93c7274161e708e9d9314a
Antoine Louis <alouis2040@gmail.com>
2026-02-03 09:15:43 +0100

    test fct generation

--- sudoku_main.cpp
+++ sudoku_main.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 int main(){
+    //test affichage et importation de grille
     Grille Test(2);
     Test.importer("grille1.txt");
     Test.afficher();
+
+    //test generation de grille
+    Grille gr(3);
+    gr.generation();
+    gr.afficher();
     return 0;
 }
